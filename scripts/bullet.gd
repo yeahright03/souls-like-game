8460740915ecde554e843c026338bf81c227817e
Martin P.J <martinjansander@gmail.com>
2025-06-29 19:21:38 +0200

    updated bullet.gd
    created a timer of 0.05 (seconds i believe) for when the bullet is unable to hit anything so that the enemy won't absorb bullet
    
    (will find better solution another time)

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
+var uselessTimer : float = 0.05
 const speed : int = 300
 
 func _process(delta: float) -> void:
 	# gives bullet speed
 	position += transform.x.normalized() * speed * delta
+
+	if uselessTimer >= 0.0:
+		uselessTimer -= delta
+
+
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	# removes bullet when outside of camera
@@ -12,6 +18,6 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 func _on_bullet_hit_body_entered(body:Node2D) -> void:
 	# prints when enemy is registering hits
-	if body.name == "generic enemy":
+	if body.name == "generic enemy" and uselessTimer <= 0.0:
 		print("HIT!")
 		queue_free()
