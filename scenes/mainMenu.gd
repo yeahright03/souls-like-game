2a9eb7e2a43b4020877ee65cc8acef8bec2df7f1
Martin P.J <martinjansander@gmail.com>
2025-07-01 21:25:07 +0200

    added simple main menu

---
+++ scenes/mainMenu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
