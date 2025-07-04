d1f5ed14c2e34c9deebdff0cc13f805b9a83563f
yeahright03 <martinjansander@gmail.com>
2025-07-04 18:31:53 +0200

    added new default area
    added templeArea01
    added sceneTransitionAnimation

--- scripts/areas/templeArea01.gd
+++ scripts/areas/templeArea01.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+@onready var sceneTransitionAnimation = $Camera2D/screenTransitionAnimation/AnimationPlayer
+
+func _on_area_exit_body_entered(body:Node2D) -> void:
+	if body.name == "player":
+		sceneTransitionAnimation.play("fadeIn")
+		await get_tree().create_timer(0.5).timeout
+		get_tree().change_scene_to_file("res://scenes/templeArea01.tscn")
