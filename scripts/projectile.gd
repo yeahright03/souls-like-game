35307bd6f4dfd1d3b495f2e70b6e607c7f3b9a30
Martin P.J <martinjansander@gmail.com>
2025-07-08 15:54:26 +0200

    made bullets alive longer

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -14,6 +14,7 @@ func _process(delta: float) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	# removes bullet when outside of camera
+	await get_tree().create_timer(2).timeout
 	queue_free()
 
 func _on_bullet_hit_body_entered(body:Node2D) -> void:
