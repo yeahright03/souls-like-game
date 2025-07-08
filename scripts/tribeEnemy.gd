ae851d08145ab9b5c3f08acc2b18b032455ec94b
Martin P.J <martinjansander@gmail.com>
2025-07-08 21:15:12 +0200

    added enabledShooting boolean

--- scripts/tribeEnemy.gd
+++ scripts/tribeEnemy.gd
@@ -5,6 +5,7 @@ const projectile = preload("res://player/spearProjectile.tscn")
 var playerSpotted : bool = false # checks for player
 var timeSinceShot : float = 1 # larger = longer time between shots
 var health = 10
+@export var enabledShooting : bool = true
 
 func _ready() -> void:
 	spearWeapon.hide()
@@ -12,7 +13,7 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	# takes playerSpotted and begin shooting at player based on enemy angle
-	if playerSpotted == true:
+	if playerSpotted == true and enabledShooting:
 		spearWeapon.look_at(get_tree().current_scene.get_node("player").position)
 		if timeSinceShot >= 1:
 			var spearFlip : int = randi_range(1, -1)
@@ -29,14 +30,12 @@ func _process(delta: float) -> void:
 			get_tree().root.add_child(projectileInstance)
 			projectileInstance.global_position = spearWeapon.global_position
 			projectileInstance.rotation = spearWeapon.rotation
-	else:
-		self.rotation = 0
 
 	# makes sure the enemy doesn't go rapid fire
 	if timeSinceShot < 1:
 		timeSinceShot += delta
 
-	if health <= 0:
+	if health <= 0 and enabledShooting:
 		queue_free()
 
 func _on_player_detection_body_entered(body: Node2D) -> void:
